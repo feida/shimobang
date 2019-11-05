034c3d097fa8cce2c9bb5654208c69d39159899d
yao <peng.yao@pcsg.net>
2019-11-05 10:42:37 +0800

    ..

--- src/views/404.vue
+++ src/views/404.vue
@@ -13,7 +13,7 @@
           <a class="link-type" href="https://wallstreetcn.com" target="_blank">盟碳</a>
         </div>
         <div class="bullshit__headline">{{ message }}</div>
-        <div class="bullshit__info">请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告</div>
+        <div class="bullshit__info">请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告!!</div>
         <a href="" class="bullshit__return-home">返回首页</a>
       </div>
     </div>
